7ea1c0266491b82befad79098d245c719e1cdabc
Ivan B <ivar110kg@gmail.com>
2022-07-04 19:51:28 +0400

    Add Repository(DAO)

---
+++ src/main/java/io/github/ivan100kg/rest/dao/EmployeeDAO.java
@@ -0,0 +1,15 @@
+package io.github.ivan100kg.rest.dao;
+
+import io.github.ivan100kg.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    void deleteEmployee(int id);
+}
